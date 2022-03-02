cc1946a1c8859a82d382287fe85ee5504fff9116
SlavicaSkoric <skoric.slavica.hr@gmail.com>
2022-03-02 12:04:09 +0100

    separate test databases

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 // app - an object that is going to take incoming HTTP requests and depending upon some information in that request (like the type, method) and the route that it's trying to go to (eg /api/drivers), it's going to run some different amount of code inside of our application - that's what the purpose of express server - application is
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber');
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber');
+}
 
 // any incoming request, assume it's json, parse it into an object so that we can work with it
 app.use(bodyParser.json());
